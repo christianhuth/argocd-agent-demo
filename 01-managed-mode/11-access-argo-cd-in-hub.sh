d45a75f55a82a87305c3f8000a30eafe1f906269
Christian Huth <christian@knell.it>
2026-02-09 15:26:52 +0100

    wip

--- 01-managed-mode/11-access-argo-cd-in-hub.sh
+++ 01-managed-mode/11-access-argo-cd-in-hub.sh
@@ -6,4 +6,4 @@ source .env
 kubectl -n $NAMESPACE_NAME get secret argocd-initial-admin-secret --context kind-$PRINCIPAL_CLUSTER_NAME \
   -o jsonpath="{.data.password}" | base64 -d && echo
 
-kubectl port-forward svc/argocd-server -n $NAMESPACE_NAME 8080:443 --context kind-$PRINCIPAL_CLUSTER_NAME &
+kubectl port-forward svc/argocd-server -n $NAMESPACE_NAME 8080:443 --context kind-$PRINCIPAL_CLUSTER_NAME
